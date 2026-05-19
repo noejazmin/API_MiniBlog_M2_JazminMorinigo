2071d3d0404b6588c198101a7958ab800a26fb07
noejazmin <noejazminmrivas@gmail.com>
2026-05-19 12:36:51 -0300

    correxion de sintaxis

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,27 +1,25 @@
-require("dotenv").config();
 const { loadEnvFile } = require('node:process');
+const { Pool } = require("pg");
 
-<<<<<<< HEAD
+
 const fs = require('node:fs');
-=======
->>>>>>> 312ba2a74ef56a7db3d0813755f70a6d076b0da9
+
     if (fs.existsSync('.env')) {
         loadEnvFile('.env');
         }
 
-const { Pool } = require("pg");
-
-const pool = process.env.DATABASE_URL
+const poolConfig = process.env.DATABASE_URL
     ? {
         connectionString: process.env.DATABASE_URL
     }
     : {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
     };
 
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
