6fa4ea006469263db5a1d8f539382258e37fb66a
noejazmin <noejazminmrivas@gmail.com>
2026-05-19 12:03:24 -0300

    conexion string realizada

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,13 +1,23 @@
 require("dotenv").config();
+const { loadEnvFile } = require('node:process');
+
+    if (fs.existsSync('.env')) {
+        loadEnvFile('.env');
+        }
 
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-});
+const pool = process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL
+    }
+    : {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    };
+
 
 module.exports = pool;
